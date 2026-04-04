aea42b1d2a2185b10b7dc0563855dc5391114d62
Karan Ram <Karanram73005@gmail.com>
2026-04-04 16:51:39 +0530

    user features created

--- CustomerServices.java
+++ CustomerServices.java
@@ -5,6 +5,20 @@ public class CustomerServices {
         System.out.println("\n\n\033[1;37mBalance: \033[0m" + "₹" + user.getBalance() + "\n");
     }
 
+    static void withdrawMoney(Scanner input, Accounts user) {
+        System.out.println("\nEnter amount to withdraw: ");
+        double amount = input.nextDouble();
+        if (amount <= 0) {
+            System.out.println("\033[31mInvalid amount!\033[0m");
+        } else if (user.getBalance() - amount <= 0) {
+            System.out.println("\n\033[1;31mInsufficient balance.\033[0m");
+
+        } else {
+            user.withdraw(amount);
+            System.out.println("\n\033[31m₹" + amount + " withdrawn successfully\033[0m\n");
+        }
+    }
+
     static void getUserInfo(Accounts user) {
         System.out.println("\n\033[1;37mName: \033[0m" + user.getName());
         System.out.println("\033[1;37mAccount Number: \033[0m" + user.getAccountNum());
@@ -52,7 +66,7 @@ public class CustomerServices {
             if (!found) {
                 System.out.println("Reciever account not found!");
             }
-            
+
             System.out.println("\n\nPress '1' to transfer more amount to other account.");
             System.out.println("<< Press any key to return to menu <<");
             System.out.print(">>> ");
